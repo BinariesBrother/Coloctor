68e2359e8b173cd090226586e80561db71abf828
mehdimakhlouf <makhloufmehdi1@gmail.com>
2018-02-18 15:36:41 +0100

    [reformat#confWebStorm] utilisation de la conf IDE

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
 
 describe('coloctor App', () => {
   let page: AppPage;
--- src/webapp/main.ts
+++ src/webapp/main.ts
@@ -1,8 +1,8 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { GlobalModule } from './app/global/global.module';
-import { environment } from './environments/environment';
+import {GlobalModule} from './app/global/global.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
